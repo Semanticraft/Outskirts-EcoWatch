8c06f4138df022b71a8be25508e61258af17a653
Dennis Forster <66913269+Semanticraft@users.noreply.github.com>
2024-05-11 22:32:15 +0200

    user JSONs and initially hiding sections

---
+++ src/users.js
@@ -0,0 +1,13 @@
+let admina = {
+    username: "admina", 
+    password: "password", 
+    role: "admin",
+    name: "Mina"
+};
+
+let normalo = {
+    username: "normalo", 
+    password: "password", 
+    role: "non-admin",
+    name: "Norman"
+};
