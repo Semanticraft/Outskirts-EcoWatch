e033eb890ade85aaf33f27cd27e2b186a36f932d
Lezhor <117937256+Lezhor@users.noreply.github.com>
2024-05-18 19:04:07 +0200

    Rewritten EVERYTHING
    too many changes for 1 commit ... sry :/

--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
-import {userList} from './users.js';
-import { locationList } from './locations.js';
+import { login, logout } from './users.js';
+import { createLocation, pushLocation, locationList } from './locations.js';
+import { getCoords } from './geoservice_api.js';
+import { openSection } from './navigation.mjs';
+
+console.log("starting main.js");
 
 let itemCounter = 0;
 let rowDiv;
-locationList.forEach(location => {
+let rowParent = document.getElementById('welcome-message');
+
+// Sets all form-fields for update-screen
+function setUpdateScreenValues(location) {
+    document.getElementById('new-name').value = location.name;
+    document.getElementById('new-description').value = location.description;
+    document.getElementById('new-street').value = location.street;
+    document.getElementById('new-house-number').value = location.houseNumber;
+    document.getElementById('new-zip').value = location.zip;
+    document.getElementById('new-city').value = location.city;
+    document.getElementById('new-category').value = location.category;
+    document.getElementById('latId').value = location.lat;
+    document.getElementById('longId').value = location.lon;
+    document.getElementById('new-temporary').checked = location.temporary;
+    document.getElementById('update-and-delete-image').src = location.imagePath;
+}
+
+// Adds location to UI
+function addLocationToList(location) {
     itemCounter++;
     // inserting row after every third element
     if(itemCounter % 3 == 1) {
+        if (itemCounter > 1) {
+            rowParent = rowDiv;
+        }
         rowDiv = document.createElement('div');
         rowDiv.classList.add('row');
     }
@@ -27,58 +52,21 @@ locationList.forEach(location => {
 
     rowDiv.appendChild(outerDiv);
 
-    let parentElement = document.getElementById('welcome-message');
-
     if (itemCounter % 3 == 1) {
-        parentElement.insertAdjacentElement('afterend', rowDiv);
+        rowParent.insertAdjacentElement('afterend', rowDiv);
     }
 
     /*Setting the detail view to fit the location values*/
     outerDiv.onclick = function() {
-        document.getElementById('new-name').value = location.name;
-        document.getElementById('new-description').value = location.description;
-        document.getElementById('new-street').value = location.street;
-        document.getElementById('new-house-number').value = location.houseNumber;
-        document.getElementById('new-zip').value = location.zip;
-        document.getElementById('new-city').value = location.city;
-        document.getElementById('new-category').value = location.category;
-        document.getElementById('latId').value = location.latitude;
-        document.getElementById('longId').value = location.longitude;
-        document.getElementById('new-temporary').checked = location.temporary;
-        document.getElementById('update-and-delete-image').src = location.imagePath;
-    }
-})
-
-function login(username, password) {
-    if (userList.has(username)) {
-        if(userList.get(username)[0] == password) {
-            document.getElementById('login').style.display = 'none';
-            document.getElementById('main').style.display = 'block';
-            document.getElementById('welcome-message').innerHTML = 'Welcome, ' + userList.get(username)[2] + '!';
-            if(userList.get(username)[1] == 'admin') {
-                document.getElementById('add-button').style.display = 'block';
-                document.getElementById('update-button').style.display = 'block';
-                document.getElementById('delete-button').style.display = 'block';
-            } else {
-                document.getElementById('add-button').style.display = 'none';
-                document.getElementById('update-button').style.display = 'none';
-                document.getElementById('delete-button').style.display = 'none';
-            }
-        } else if(password != "") {
-            alert('The password is incorrect.');
-        }
-    } else if(username != "") {
-        alert('The username does not exist.');
+        setUpdateScreenValues(location);
+        openSection('update-and-delete');
     }
 }
 
-function logout() {
-    let sections = document.getElementsByTagName('section');
-    for (let i = 1; i < sections.length; i++) {
-        sections[i].style.display = 'none';
-    }
-    document.getElementById('login').style.display = 'block';
-}
+locationList.forEach(addLocationToList)
+
+
+// --------------------- SETUP EVENT-HANDLERS -----------------------
 
 document.getElementById('login-form').onsubmit = function() {
     login(document.getElementById('username').value, document.getElementById('password').value);
@@ -87,25 +75,37 @@ document.getElementById('login-form').onsubmit = function() {
 
 document.getElementById('logout-button').onclick = logout;
 
-let locations = document.getElementsByClassName('location');
-Array.from(locations).forEach(location => {
-    location.onclick = function() { 
-        document.getElementById('main').style.display = 'none';
-        document.getElementById('update-and-delete').style.display = 'block';
-    }
-});
-
 document.getElementById('cancel-update-and-delete').onclick = function() {
-    document.getElementById('main').style.display = 'block';
-    document.getElementById('update-and-delete').style.display = 'none';
+    openSection('main');
 }
 
 document.getElementById('add-button').onclick = function() {
-    document.getElementById('main').style.display = 'none';
-    document.getElementById('add').style.display = 'block';
+    openSection('add');
 }
 
 document.getElementById('cancel-add').onclick = function() {
-    document.getElementById('add').style.display = 'none';
-    document.getElementById('main').style.display = 'block';
+    openSection('main');
+}
+
+document.getElementById('add-form').onsubmit = async function() {
+    const location = createLocation(
+        document.getElementById("add_name").value,
+        document.getElementById("add_description").value,
+        document.getElementById("add_address_street").value,
+        document.getElementById("add_address_no").value,
+        document.getElementById("add_address_zip").value,
+        document.getElementById("add_address_city").value,
+        document.getElementById("add_category").value,
+        document.getElementById("add_temporary_check").checked,
+        document.getElementById("imagesUpload").src
+    )
+    if (await getCoords(location)) {
+        console.log("Loaded coordinates for location: " + JSON.stringify(location));
+        pushLocation(location);
+        addLocationToList(location);
+        openSection('main');
+    } else {
+        console.log("Failed to getCoords() for location: " + JSON.stringify(location));
+    }
+    return false;
 }
--- src/users.js
+++ src/users.js
@@ -1,10 +1,66 @@
+import { openSection } from "./navigation.mjs";
+
 const userList = new Map([]);
 
+var loggedIn = false;
+var user = null;
+
+// Adds new user to userList - if user does not exist yet
 function addUser(username, password, role, name) {
-    userList.set(username, [password, role, name]);
+    let userObj = {
+        username: username, 
+        password: password, 
+        role: role, 
+        name: name
+    }
+    if (userExists(username)) {
+        console.log('Username"' + username + "' already exists!");
+    } else {
+        userList.set(username, userObj);
+    }
 }
 
+function userExists(username) {
+    return userList.has(username);
+}
+
+// hardcoded users
 addUser('admina', 'password', 'admin', 'Mina');
 addUser('normalo', 'password', 'non-admin', 'Norman');
 
-export {userList, addUser};
+// checks the login data and opens main section
+function login(username, password) {
+    if (userExists(username)) {
+        console.log("username correct");
+        console.log("user: " + userList.get(username));
+        if (userList.get(username).password === password) {
+            console.log("password correct");
+            loggedIn = true;
+            user = userList.get(username);
+            
+            // Login successful!!!
+            openSection('main');
+            document.getElementById('welcome-message').innerHTML = 'Welcome, ' + user.name + '!';
+
+            let status = (user.role == "admin") ? 'block' : 'none';
+            document.getElementById('add-button').style.display = status;
+            document.getElementById('update-button').style.display = status;
+            document.getElementById('delete-button').style.display = status
+
+            alert("successful login");
+        } else {
+            alert("wrong password");
+        }
+    } else {
+        alert("username '" + username + "' does not exist")
+    }
+}
+
+// opens login section
+function logout() {
+    loggedIn = false;
+    user = null;
+    openSection('login');
+}
+
+export {addUser, login, logout};
