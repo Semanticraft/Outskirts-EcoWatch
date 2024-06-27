33f198c78d58c12719b1921e81d4c8d1929eb464
Lezhor <117937256+Lezhor@users.noreply.github.com>
2024-06-28 00:12:14 +0200

    Login now hooked up to backend
    - removed hardcoded users

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,33 +1,7 @@
+import { postLogin } from "./api.js";
 import { openSection } from "./navigation.mjs";
 import { disableElementsByClass } from "./util.js";
 
-const userList = new Map([]);
-
-var loggedIn = false;
-var user = null;
-
-// Adds new user to userList - if user does not exist yet
-function addUser(username, password, role, name) {
-    let userObj = {
-        username: username, 
-        password: password, 
-        role: role, 
-        name: name
-    }
-    if (userExists(username)) {
-        console.log('Username"' + username + "' already exists!");
-    } else {
-        userList.set(username, userObj);
-    }
-}
-
-function userExists(username) {
-    return userList.has(username);
-}
-
-// hardcoded users
-addUser('admina', 'password', 'admin', 'Mina');
-addUser('normalo', 'password', 'non-admin', 'Norman');
 
 // checks the login data and opens main section
 function login(username, password) {
@@ -37,38 +11,29 @@ function login(username, password) {
     401: alert "wrong credentials", show login page
     error: alert "something wrong", show login page
     */
-    if (userExists(username)) {
-        console.log("username correct");
-        console.log("user: " + JSON.stringify(userList.get(username)));
-        if (userList.get(username).password === password) {
-            console.log("password correct");
-            loggedIn = true;
-            user = userList.get(username);
-            
-            // Login successful!!!
-            openSection('main');
-            document.getElementById('welcome-message').innerHTML = 'Welcome, ' + user.name + '!';
-            
-            let isAdmin = user.role == "admin";
-            let status = isAdmin ? 'block' : 'none';
-            document.getElementById('add-button').style.display = status;
-            document.getElementById('update-button').style.display = status;
-            document.getElementById('delete-button').style.display = status;
+   //console.log("Login! username: " + username + ", password: " + password)
+   postLogin(username, password)
+    .then(user => {
+        
+        // Login successful!!!
+        openSection('main');
+        document.getElementById('welcome-message').innerHTML = 'Welcome, ' + user.name + '!';
+        
+        let isAdmin = user.role == "admin";
+        let status = isAdmin ? 'block' : 'none';
+        document.getElementById('add-button').style.display = status;
+        document.getElementById('update-button').style.display = status;
+        document.getElementById('delete-button').style.display = status;
 
-            disableElementsByClass('update-input', !isAdmin);
-        } else {
-            alert("wrong password");
-        }
-    } else {
-        alert("username '" + username + "' does not exist")
-    }
+        disableElementsByClass('update-input', !isAdmin);
+    }).catch(error => {
+        alert("Wrong username or password!");
+    })
 }
 
 // opens login section
 function logout() {
-    loggedIn = false;
-    user = null;
     openSection('login');
 }
 
-export {addUser, login, logout};
+export {login, logout};
